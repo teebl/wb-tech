66e1b1a135a39679e9bdbdaedb49c27ed160d128
Trevor Seibel <trevor.seibel@fullscript.com>
2022-05-08 17:42:03 -0500

    input component

---
+++ src/components/shared/form/Input/Input.tsx
@@ -0,0 +1,16 @@
+import React, {
+  FC,
+  HTMLInputTypeAttribute,
+  InputHTMLAttributes,
+  ReactNode,
+} from "react";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  children?: ReactNode | ReactNode[];
+}
+
+const Input: FC<Props> = ({ children, ...rest }) => {
+  return <input {...rest}>{children}</input>;
+};
+
+export { Input };
