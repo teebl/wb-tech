e3eafc096776db75eaf7f7f9fb2a560be5143e14
Trevor Seibel <trevor.seibel@fullscript.com>
2022-05-08 14:37:26 -0500

    navbar init

--- src/components/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from "react";
 import { Outlet } from "react-router-dom";
+import { Navbar } from "../../navigation";
 
 const DefaultLayout: FC = () => {
   return (
     <>
-      nav goes here
+      <Navbar />
       <main>
         <Outlet />
       </main>
---
+++ src/components/navigation/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Navbar: FC = () => {
+  return <nav>Wrapbook</nav>;
+};
+
+export { Navbar };
