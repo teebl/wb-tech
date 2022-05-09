e2ca165a8dd7cf33c04dc3706a7ea326b80bb5fd
Trevor Seibel <trevor.seibel@fullscript.com>
2022-05-08 20:29:51 -0500

    dashboard layout

--- src/components/dashboard/PeopleList/PeopleList.tsx
+++ src/components/dashboard/PeopleList/PeopleList.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from "react";
+import { Box } from "../../shared/composition";
 
 const PeopleList: FC = () => {
-  return <div>list of people</div>;
+  return (
+    <div className="flex-grow-2 flex flex-col gap-6 w-100">
+      <Box className="min-w-[20rem]">Person 1</Box>
+      <Box>Person 2</Box>
+      <Box>Person 3</Box>
+    </div>
+  );
 };
 
 export { PeopleList };
--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -2,11 +2,17 @@ import React, { FC } from "react";
 import { PeopleList, InvitePeople } from "../components/dashboard";
 
 const Dashboard: FC = () => (
-  <div>
-    <h1>Your company Dashboard</h1>
-    <p>Manage people, view history &amp; see how your projects are doing.</p>
-    <PeopleList />
-    <InvitePeople />
+  <div className="pt-12 min-h-full">
+    <section className="mb-12">
+      <h1>Your company Dashboard</h1>
+      <h3>
+        Manage people, view history &amp; see how your projects are doing.
+      </h3>
+    </section>
+    <section className="flex gap-6 flex-row w-full justify-between">
+      <PeopleList />
+      <InvitePeople />
+    </section>
   </div>
 );
 
