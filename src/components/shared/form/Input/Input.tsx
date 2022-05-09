096d90f341218a0792a827423bf9ba86bd398823
Trevor Seibel <trevor.seibel@fullscript.com>
2022-05-08 19:57:16 -0500

    input styling

--- src/components/shared/form/Input/Input.tsx
+++ src/components/shared/form/Input/Input.tsx
@@ -1,11 +1,27 @@
 import React, { FC, InputHTMLAttributes, ReactNode } from "react";
+import { mergeClassNames } from "../../../../helpers";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   children?: ReactNode | ReactNode[];
 }
 
-const Input: FC<Props> = ({ children, ...rest }) => {
-  return <input {...rest}>{children}</input>;
+const Input: FC<Props> = ({ children, className, ...rest }) => {
+  const classNames = mergeClassNames(
+    className,
+    "py-3",
+    "px-4",
+    "block",
+    "my-4",
+    "border-1",
+    "rounded-xl",
+    "bg-slate-100",
+    "w-full"
+  );
+  return (
+    <input className={classNames} {...rest}>
+      {children}
+    </input>
+  );
 };
 
 export { Input };
